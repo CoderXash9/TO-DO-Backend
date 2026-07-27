047eb4c5633f9e0c2b8086d4a6587b44bca3eaa5
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-27 21:18:03 +0530

    added admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "priority",
+        "status",
+        "due_date",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
+
+    list_filter = (
+        "priority",
+        "status",
+        "due_date",
+    )
+
+    ordering = ("due_date",)
+
+    list_per_page = 10
--- tasks/models.py
+++ tasks/models.py
@@ -33,3 +33,5 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+    
