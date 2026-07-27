db67a255d39904c2f8f5bb330bf916e4bb02ce9b
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-27 21:37:02 +0530

    added views.py to the project

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+
+    serializer_class = TaskSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "priority",
+        "status",
+    ]
+
+    search_fields = [
+        "title",
+        "description",
+    ]
+
+    ordering_fields = "__all__"
