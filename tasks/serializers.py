88f1ad446dc07444e1b0abd1f62569a0b097d4f8
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-27 22:42:48 +0530

    solved a bug in validation

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -7,17 +7,15 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = "__all__"
 
-        def validate_title(self, value):
-            if len(value.strip()) < 3:
-                raise serializers.ValidationError(
-                    "Title must contain atleast 3 characters"
-                )
+    def validate_title(self, value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError("Title must contain atleast 3 characters")
 
-            return value
+        return value
 
-        def validate_due_date(self, value):
-            from datetime import date
+    def validate_due_date(self, value):
+        from datetime import date
 
-            if value < date.today():
-                raise serializers.ValidationError("Due date cannot be in the past.")
-            return value
+        if value < date.today():
+            raise serializers.ValidationError("Due date cannot be in the past.")
+        return value
